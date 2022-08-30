ae3e19ad1d29fea8f33f8cfaa647a1b7ef8e3e13
Ahmet Talha Şengün <97346035+AhmetTalhaSengun@users.noreply.github.com>
2022-08-30 15:00:54 +0300

    Firebase Entegre ve Yönetici Kullanıcı Kaydı
    Firebase ile entegresi sağlandı ve yöneticiler için Firebase hafızasında sınıf oluşturuldu ve üye kaydı sağlandı. Üye girişi ara yüzü tasarlandı.

--- app/src/main/java/com/example/yttest/AdminLogInActivity.java
+++ app/src/main/java/com/example/yttest/AdminLogInActivity.java
@@ -2,7 +2,10 @@ package com.example.yttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AdminLogInActivity extends AppCompatActivity {
 
@@ -11,4 +14,21 @@ public class AdminLogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_log_in);
     }
+
+
+
+    public void SignInSeleckted (View view){
+
+        TextView LogInText = (TextView)findViewById(R.id.LogInText);
+
+        LogInText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminLogInActivity.this, AdminSignUpActivity.class));
+            }
+        });
+
+    }
+
+
 }
--- app/src/main/java/com/example/yttest/AdminSignUpActivity.java
+++ app/src/main/java/com/example/yttest/AdminSignUpActivity.java
@@ -1,19 +1,73 @@
 package com.example.yttest;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.example.yttest.databinding.ActivityAdminSignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+
 
 public class AdminSignUpActivity extends AppCompatActivity {
-
+    private ActivityAdminSignUpBinding binding;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_sign_up);
+        binding = ActivityAdminSignUpBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        auth= FirebaseAuth.getInstance();
+
     }
+
+
+
+    public void SignUpClick (View view){
+
+        String email=binding.EnterEmailText.getText().toString();
+        String password=binding.PasswordText.getText().toString();
+
+        if(email.equals("") || password.equals("")){
+            Toast.makeText(this,"Email ve şifrenizi giriniz.",Toast.LENGTH_LONG).show();
+        } else{
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(AdminSignUpActivity.this,AdminLogInActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(AdminSignUpActivity.this,e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                }
+            });
+
+        }
+
+
+    }
+
 
     public void LogInSeleckted (View view){
 
@@ -27,5 +81,6 @@ public class AdminSignUpActivity extends AppCompatActivity {
         });
 
     }
+
 
 }
